c219e02c8cc6616ec1ad03c555389f6344c99cfe
Ashwin Vijendra <ashwinvijendra@gmail.com>
2022-03-14 15:43:52 +0530

    simple to-do checkbox and delete

---
+++ src/components/Content.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { FaTrashAlt } from "react-icons/fa";
+
+const Content = () => {
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      name: "Get bike serviced",
+      checked: false,
+    },
+    {
+      id: 2,
+      name: "Get car repaired",
+      checked: false,
+    },
+    {
+      id: 3,
+      name: "Get cycle rented",
+      checked: true,
+    },
+  ]);
+
+  const handleChange = (id) => {
+    const listItems = items.map((item) => {
+      return item.id === id ? { ...item, checked: !item.checked } : item;
+    });
+    setItems(listItems);
+    localStorage.setItem("shopping list", JSON.stringify(listItems));
+  };
+
+  const handleDelete = (id) => {
+    const listItems = items.filter((item) => item.id !== id);
+    setItems(listItems);
+    localStorage.setItem("shopping list", JSON.stringify(listItems));
+  };
+
+  return (
+    <main style={{ minHeight: "calc(100vh - 38px)" }}>
+      {items.length ? (
+        <ol>
+          {items.map((item) => (
+            <li className="item" key={item.id}>
+              <input
+                type="checkbox"
+                checked={item.checked}
+                onChange={() => handleChange(item.id)}
+              />
+              <label
+                style={{ textDecoration: item.checked ? "line-through" : null }}
+              >
+                {item.name}
+              </label>
+              <FaTrashAlt onClick={() => handleDelete(item.id)} />
+            </li>
+          ))}
+        </ol>
+      ) : (
+        <p>Your list is empty !</p>
+      )}
+    </main>
+  );
+};
+
+export default Content;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header style={{ background: "mediumblue", color: "#fff" }}>Header</header>
+  );
+};
+
+export default Header;
